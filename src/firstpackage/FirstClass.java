3d3dfe7bb6a5c033c2d601452a47b41afef1e316
aliAka <r.ali34aka@gmail.com>
2020-11-07 18:15:52 +0000

    third  commit from local master

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -7,7 +7,9 @@ public class FirstClass {
 		 //Specimen!
 		System.out.println("first commit from locam machine");
 		System.out.println("Second line of code I am typing");
- 
+		System.out.println("Third commit from local master");
 
 	}
 }
+
+
