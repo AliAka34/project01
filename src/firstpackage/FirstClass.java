7626802c184b84bdb52175ea655109737b04287d
aliAka <r.ali34aka@gmail.com>
2020-11-07 17:07:49 +0000

    first commit from local master

---
+++ src/firstpackage/FirstClass.java
@@ -0,0 +1,13 @@
+package firstpackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		 //Specimen!
+		System.out.println("first commit from locam machine");
+
+ 
+
+	}
+}
